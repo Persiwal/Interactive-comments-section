0ebe8b040a7e190f805a58f4a4ac5a08c6fa8290
Mateusz Barwicki <mateuszbarwicki2@gmail.com>
2022-05-16 17:05:33 +0200

    Add import and delete fetchComments function
    Since the data is stored locally in data.sjon file, it's better to just import it to main script file instead of using fetch

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+import data from "./data.json" assert { type: "json" };
+
 const commentsList = document.querySelector(".comments-list");
 const addCommentAvatar = document.querySelector(".add-comment__avatar");
 const addCommentText = document.querySelector(".add-comment__text");
@@ -5,29 +7,11 @@ const addCommentButton = document.querySelector(".add-comment__cta");
 const addCommentSection = document.querySelector(".add-comment");
 const invalidInput = document.querySelector(".add-comment__invalid-input");
 
-let addedCommentsArray = [];
+let comments = [...data.comments];
+let currentUser = { ...data.currentUser };
 
-//fetch data from data.json file
-const fetchComments = async () => {
-  try {
-    const response = await fetch("./data.json");
-    const data = await response.json();
-    return data;
-  } catch (e) {
-    console.log(e);
-  }
-};
-
-const displayComments = async (addedCommentsArray) => {
+const displayComments = () => {
   commentsList.innerHTML = "";
-  const data = await fetchComments();
-  console.log(data);
-  let comments = data.comments;
-  const currentUser = data.currentUser;
-
-  if (addedCommentsArray.length > 0) {
-    comments = [...data.comments, ...addedCommentsArray];
-  }
 
   comments.map((comment) => {
     commentsList.innerHTML += `<li class="comments-list__comment">
@@ -60,12 +44,12 @@ const displayComments = async (addedCommentsArray) => {
               data-id="${comment.id}"
             />
             </div>
-            <p class="comments-list__score-number" data-id="${comment.id}">${
-      comment.score
-    }</p>
-    <div class="comments-list__plus ${
-      comment.user.username === currentUser.username ? "disable" : ""
-    }">
+            <p class="comments-list__score-number" data-id="${comment.id}">
+            ${comment.score}
+            </p>
+            <div class="comments-list__plus ${
+              comment.user.username === currentUser.username ? "disable" : ""
+            }">
             <img
               src="./images/icon-plus.svg"
               alt=""
@@ -74,10 +58,19 @@ const displayComments = async (addedCommentsArray) => {
             </div>
           </div>
           <div class="comments-list__actions">
-          <div class="reply-button" data-id="${comment.id}">
-            <img src="./images/icon-reply.svg" alt="reply icon" />
-            <span>Reply</span>
-            </div>
+          ${
+            comment.user.username !== currentUser.username
+              ? `<div class="reply-button" data-id="${comment.id}">
+            <img src="./images/icon-reply.svg" alt="reply icon" data-id="${comment.id}"/>
+            <span data-id="${comment.id}">Reply</span>
+            </div>`
+              : `<div class="edit-button" data-id="${comment.id}" data-id="${comment.id}">
+            <img src="./images/icon-edit.svg" alt="edit icon" data-id="${comment.id}"/>
+            <span data-id="${comment.id}">Edit</span></div>
+            <div class="delete-button" data-id="${comment.id}">
+            <img src="./images/icon-delete.svg" alt="delete icon" data-id="${comment.id}"/>
+            <span data-id="${comment.id}">Delete</span></div>`
+          }
           </div>
         </li>`;
 
@@ -117,9 +110,9 @@ const displayComments = async (addedCommentsArray) => {
             <p class="comments-list__score-number" data-id="${reply.id}">${
           reply.score
         }</p>
-    <div class="comments-list__plus ${
-      reply.user.username === currentUser.username ? "disable" : ""
-    }">
+            <div class="comments-list__plus ${
+              reply.user.username === currentUser.username ? "disable" : ""
+            }">
             <img
               src="./images/icon-plus.svg"
               alt=""
@@ -128,47 +121,111 @@ const displayComments = async (addedCommentsArray) => {
             </div>
             </div>
           <div class="comments-list__actions">
-          <div class="reply-button" data-id="${reply.id}">
-            <img src="./images/icon-reply.svg" alt="reply icon" />
-            <span>Reply</span>
-            </div>
+          ${
+            reply.user.username !== currentUser.username
+              ? `<div class="reply-button" data-id="${reply.id}">
+            <img src="./images/icon-reply.svg" alt="reply icon" data-id="${reply.id}/>
+            <span data-id="${reply.id}>Reply</span>
+            </div>`
+              : `<div class="edit-button" data-id="${reply.id}">
+              <img src="./images/icon-edit.svg" alt="edit icon" data-id="${reply.id}/>
+              <span data-id="${reply.id}>Edit</span>
+              </div>
+            <div class="delete-button" data-id="${reply.id}" >
+              <img src="./images/icon-delete.svg" alt="delete icon" data-id="${reply.id}/>
+              <span data-id="${reply.id}>Delete</span>
+            </div>`
+          }
           </div>
         </li>`;
       });
     }
-    // add listeners to score buttons
-    const minusButtons = document.querySelectorAll(".comments-list__minus");
-    const scores = document.querySelectorAll(".comments-list__score-number");
-    const plusButtons = document.querySelectorAll(".comments-list__plus");
+  });
 
-    minusButtons.forEach((button) =>
-      button.addEventListener("click", (event) => {
-        event.preventDefault();
-        scores.forEach((score) => {
-          if (
-            event.target.dataset.id == score.dataset.id &&
-            !score.classList.contains("disable")
-          ) {
-            score.innerHTML--;
-          } else {
-            return;
+  // add listeners to score buttons
+  const minusButtons = document.querySelectorAll(".comments-list__minus");
+  const scores = document.querySelectorAll(".comments-list__score-number");
+  const plusButtons = document.querySelectorAll(".comments-list__plus");
+  const replyButtons = document.querySelectorAll(".reply-button");
+  const editButtons = document.querySelectorAll(".edit-button");
+  const deleteButtons = document.querySelectorAll(".delete-button");
+
+  minusButtons.forEach((button) =>
+    button.addEventListener("click", (event) => {
+      event.preventDefault();
+      scores.forEach((score) => {
+        if (
+          event.target.dataset.id == score.dataset.id &&
+          !score.classList.contains("disable")
+        ) {
+          console.log(!score.classList.contains("disable"));
+          score.innerHTML--;
+        } else {
+          return;
+        }
+      });
+    })
+  );
+
+  plusButtons.forEach((button) =>
+    button.addEventListener("click", (event) => {
+      event.preventDefault();
+      console.log(event.target.dataset.id);
+      scores.forEach((score) => {
+        if (
+          event.target.dataset.id == score.dataset.id &&
+          !score.classList.contains("disable")
+        ) {
+          score.innerHTML++;
+        } else {
+          return;
+        }
+      });
+    })
+  );
+
+  editButtons.forEach((button) => {
+    button.addEventListener("click", (event) => {
+      event.preventDefault();
+      comments.forEach((comment) => {
+        if (event.target.dataset.id == comment.id) {
+          console.log("xd");
+        }
+      });
+    });
+  });
+
+  replyButtons.forEach((button) => {
+    button.addEventListener("click", (event) => {
+      event.preventDefault();
+      comments.forEach((comment) => {
+        if (event.target.dataset.id == comment.id) {
+          console.log("xd");
+        }
+      });
+    });
+  });
+
+  deleteButtons.forEach((button) => {
+    button.addEventListener("click", (event) => {
+      event.preventDefault();
+      comments.forEach((comment) => {
+        if (event.target.dataset.id == comment.id) {
+          let commentIndex = comments.indexOf(comment);
+          comments = comments.splice(commentIndex, 1);
+          displayComments();
+        }
+        comment.replies.forEach((reply) => {
+          if (event.target.dataset.id == reply.id) {
+            console.log(reply.id);
+            let commentIndex = comment.replies.indexOf(reply);
+            console.log(commentIndex);
+            comment.replies = comment.replies.splice(commentIndex, 1);
+            displayComments();
           }
         });
-      })
-    );
-
-    plusButtons.forEach((button) =>
-      button.addEventListener("click", (event) => {
-        event.preventDefault();
-        scores.forEach((score) => {
-          if (event.target.dataset.id == score.dataset.id) {
-            score.innerHTML++;
-          } else {
-            return;
-          }
-        });
-      })
-    );
+      });
+    });
   });
 
   // set current user avatar on add-comment section
@@ -176,37 +233,38 @@ const displayComments = async (addedCommentsArray) => {
               src="${currentUser.image.png}"
               alt=""
             />`;
+
+  //clear error message when typing
+  addCommentText.addEventListener("keyup", () => {
+    invalidInput.innerText = "";
+  });
+
+  // adding comment logic
+  addCommentButton.addEventListener("click", async (e) => {
+    e.preventDefault();
+    if (addCommentText.value === "") {
+      //show error when input is empty
+      invalidInput.innerText = "You can't post empty comment!";
+    } else {
+      // const data = await fetchComments();
+      // const { currentUser } = data;
+      // console.log(currentUser);
+      const newComment = {
+        id: Math.random(),
+        content: addCommentText.value,
+        createdAt: "5 months ago",
+        replies: [],
+        score: 0,
+        user: {
+          image: { png: currentUser.image.png },
+          username: currentUser.username,
+        },
+      };
+      addCommentText.value = "";
+      comments.push(newComment);
+      displayComments();
+    }
+  });
 };
 
-//clear error message when typing
-addCommentText.addEventListener("keyup", () => {
-  invalidInput.innerText = "";
-});
-
-// adding comment logic
-addCommentButton.addEventListener("click", async (e) => {
-  e.preventDefault();
-  if (addCommentText.value === "") {
-    //show error when input is empty
-    invalidInput.innerText = "You can't post empty comment!";
-  } else {
-    const data = await fetchComments();
-    const { currentUser } = data;
-    console.log(currentUser);
-    const newComment = {
-      id: Math.random(),
-      content: addCommentText.value,
-      createdAt: "5 months ago",
-      replies: [],
-      score: 0,
-      user: {
-        image: { png: currentUser.image.png },
-        username: currentUser.username,
-      },
-    };
-    addedCommentsArray.push(newComment);
-    displayComments(addedCommentsArray);
-  }
-});
-
-displayComments(addedCommentsArray);
+displayComments();
